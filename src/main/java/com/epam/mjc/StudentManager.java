e1e4ae1f15d0f9062c82a7b36c5754864a2541af
Mykhailo Derecha <don.miggel@yahoo.com>
2023-04-12 20:37:09 +0300

    custom exception done

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,12 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws IllegalArgumentException {
+    Student student = Student.getValueOf(studentID);
+    if (student == null)
+      throw new WrongParameterException(studentID);
+    return student;
+
   }
 
   public static void main(String[] args) {
